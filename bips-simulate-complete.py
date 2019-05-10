161825f99cd6f42f4e99ea53e574b17369f930a1
Mohammad Dorgham <muhammad.dorgham@gmail.com>
2019-05-10 22:36:04 +0200

    simulations for complete graphs

--- bips-simulate-complete.py
+++ bips-simulate-complete.py
@@ -7,28 +7,18 @@ Created on Sun Apr 21 15:14:37 2019
 """
 
 ###############################################################################
-####            k-BIPS simulations for toroidal graphs                    #####
+####        k-BIPS simulations for barbell-like regular graphs            #####
 ###############################################################################
 
 import matplotlib.pyplot as plt
 from pylab import MaxNLocator
-from IPython import get_ipython
 from collections import OrderedDict
-import networkx as nx
 import random
 import math
+import gc
 
 
-ipy = get_ipython()
-if ipy is not None:
-    ipy.run_line_magic('matplotlib', 'inline')
-plt.close()
-
-
-dim = 1
-n_rows=400
-n_cols=400
-n=n_rows*n_cols
+n = 1000000
 k=2
 simulations_repetition = 10
 max_increase = 0
@@ -38,19 +28,19 @@ fig_idx = 1
 
 k_avgInfecTime_map = OrderedDict()
 k_netIncrease_map = OrderedDict()
-y_ticks = [math.log2(n), math.log2(n)**2, math.log2(n)**3]
-y_ticks_labels = ['$\log\ n$', '$\log^2\ n$', '$\log^3\ n$']
+y_ticks = [math.log2(math.log2(n)), math.log2(n), math.log2(n)**2]
+y_ticks_labels = ['$\log\ \log\ n$', '$\log\ n$', '$\log^2\ n$']
 
+G_neighbors = dict()
 
-print('\nGenerating %d-D toroidal grid graph of %d nodes....' %(dim+1,n))
-
-G=nx.grid_graph(dim=[dim, n_rows, n_cols], periodic=True)
+print('\nGenerating barbell-like regular graph of %d nodes....' %(n))
+G_nodes = [v for v in range(n)]
 
 print('Done generating the graph.')  
-all_nodes = list(G.nodes)
+all_nodes = G_nodes
 
 
-while k <= 4:
+while k < n:
     infection_times_per_k = []
     net_increases_per_k = []
     
@@ -65,33 +55,18 @@ while k <= 4:
         #******************* Begining of the k-BIPS process *******************#
         print('%d-BIPS process is running now...'%k)
         net_increase_per_round = []
-        while(len(infected_set) != len(all_nodes)):
+        while(len(infected_set) != n):
             prev_infec_set_size = len(infected_set)
             newly_infected = set()
             newly_uninfected = set()
             t += 1
             
-            if prev_infec_set_size < len(all_nodes)/3:
-                nodes_to_loop_over = set(infected_set)
-                for v in infected_set:
-                    for u in G.neighbors(v):
-                        nodes_to_loop_over.add(u)
-            else:
-                nodes_to_loop_over = set(all_nodes)
             #each node samples k random neighbors
-            for node in nodes_to_loop_over:
+            for node in all_nodes:
                 if node == persistent_node:
                     continue
                 
-                neighbors = list(G.neighbors(node))
-                has_infected_neighbor = False
-                if node not in infected_set:
-                    for v in neighbors:
-                        if v in infected_set:
-                            has_infected_neighbor = True
-                            break
-                    if not has_infected_neighbor:
-                        continue    #no need to sample neighbors of this node
+                neighbors = [v for v in range(n) if v != node]
                 sampled_infected_neighbor = False
                 for _ in range(k):
                     sampled_neighbor = random.choice(neighbors)
@@ -109,6 +84,9 @@ while k <= 4:
             net_increase_per_round.append(len(infected_set)-prev_infec_set_size)
             if len(infected_set)-prev_infec_set_size > max_increase:
                 max_increase = len(infected_set)-prev_infec_set_size
+                
+            del neighbors
+            gc.collect()
         
         print('Number of rounds until %d nodes are infected: %d\n' %(n, t))
         infection_times_per_k.append(t)
@@ -119,22 +97,23 @@ while k <= 4:
     k_avgInfecTime_map[k] = round((sum(infection_times_per_k) / len(infection_times_per_k)))
     k_netIncrease_map[k] = net_increases_per_k
             
-    k += 1
+    k *= 2
     
     
 fig1 = plt.figure(fig_idx, figsize=(9,7.2))
 plt.plot(k_avgInfecTime_map.keys(), k_avgInfecTime_map.values(), 'b--', label='average infection time')
-plt.plot(k_avgInfecTime_map.keys(), [math.log2(n)**2/ math.log2(i) for i in k_avgInfecTime_map.keys()], 'g-', label='$\log^2\ n\ /\ \log\ k$')
+plt.plot(k_avgInfecTime_map.keys(), [math.log2(n)/ math.log2(i) for i in k_avgInfecTime_map.keys()], 'r-', label='$\log\ n\ /\ \log\ k$')
 plt.yscale('log')
 plt.yticks(y_ticks, y_ticks_labels)
+#plt.xlim(left=1)
 xa = plt.gca().get_xaxis()
 xa.set_major_locator(MaxNLocator(integer=True))
 plt.xlabel('Branching factor (k)')
 plt.ylabel('Average infection time (in rounds)\nlogarithmic scale')
-plt.title('k-BIPS on %d-D toroidal grid graph with %d nodes'%(dim+1,n))
+plt.title('k-BIPS on complete graphs with %d nodes'%n)
 plt.grid(True)
 plt.legend(loc='best')
-fig1.savefig('bips_toroidal_fixed-d_n'+str(n)+'.png', bbox_inches='tight')
+fig1.savefig('bips_completegraph_var-k_n'+str(n)+'.png', bbox_inches='tight')
 plt.close(fig1)
 
     
